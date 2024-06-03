6ccbe203cf05c79da44c204bf103d66a270262a1
justine-black <jybeano@gmail.com>
2024-06-03 16:54:02 +0800

    test commit - justine

--- Scripts/AndroidRun1/Script1717398460386.groovy
+++ Scripts/AndroidRun1/Script1717398460386.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-Mobile.startApplication('/Users/carlmalubag/Downloads/General-Store.apk', true)
+Mobile.startApplication('../General-Store.apk', true)
 
 Mobile.pressBack()
 
